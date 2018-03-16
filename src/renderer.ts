bc5d04fb09732142a326b4d11908387c0c0406c7
Alan Dragicevich <aldragicevich@gmail.com>
2018-03-17 11:48:44 +1300

    Init commit

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,3 +1,11 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+(() => {
+  const body: HTMLElement | null = document.querySelector("body");
+
+  if (body) {
+    body.innerHTML = "<h1>Hello World</h1>";
+  }
+})();
